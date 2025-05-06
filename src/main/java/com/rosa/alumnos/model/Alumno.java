b2c61456a6566e822adf9f6d02d4b37230777d4f
Rosa218 <18620216@tlaxiaco.tecnm.mx>
2025-05-06 09:33:05 -0600

    Act1-Conexion hacia la BD MySQL

---
+++ src/main/java/com/rosa/alumnos/model/Alumno.java
@@ -0,0 +1,17 @@
+package com.rosa.alumnos.model;
+
+import jakarta.persistence.Entity;
+
+@Entity
+public class Alumno {
+
+    private long id;
+    private string NumeroControl
+    private String nombre;
+    private String apellidos;
+    private String email;
+
+
+    
+
+}
